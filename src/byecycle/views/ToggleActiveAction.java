67466d755e46a5250fe1e516dab4305e0d9a1264
Dennis Cheung <hkdennis2k@gmail.com>
2005-07-30 02:44:48 +0000

    IViewActionDelegate class for toggle button

---
+++ src/byecycle/views/ToggleActiveAction.java
@@ -0,0 +1,34 @@
+package byecycle.views;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+public class ToggleActiveAction implements IViewActionDelegate,
+        IPropertyListener {
+    ByecycleView _viewpart;
+
+    IAction _action;
+
+    public void init(IViewPart view) {
+        _viewpart = (ByecycleView) view;
+        _viewpart.addPropertyListener(this);
+    }
+
+    public void run(IAction action) {
+        _viewpart.toggleActive(action.isChecked());
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
+
+    public void propertyChanged(Object source, int propId) {
+        assert _viewpart == source;
+        if (ByecycleView.ACTIVACITY == propId)
+            return;
+        _action.setChecked(_viewpart.isPaused());
+    }
+
+}
