0d2624f9cdaed921895172b9fb4d3d71f1affabe
Dennis Cheung <hkdennis2k@gmail.com>
2005-07-30 02:43:51 +0000

    IViewActionDelegate class for context menu(show in byecycle).

---
+++ src/byecycle/views/SelectElementAction.java
@@ -0,0 +1,39 @@
+/*
+ * Created on 2005年7月30日
+ * $id$
+ */
+package byecycle.views;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+public class SelectElementAction implements IViewActionDelegate {
+
+    public void init(IViewPart view) {
+    }
+
+    public void run(IAction action) {
+        if (_selection == null)
+            return;
+        final IWorkbenchPage activePage = PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow().getActivePage();
+        try {
+            ByecycleView _viewpart = (ByecycleView) activePage.showView(ByecycleView.PERSPECTIVE_ID);
+            activePage.activate(_viewpart);
+            _viewpart.selectionChanged(_selection);
+        } catch (PartInitException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ISelection _selection;
+
+    public void selectionChanged(IAction action, ISelection selection) {
+        _selection = selection;
+    }
+}
