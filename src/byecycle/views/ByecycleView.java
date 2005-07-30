ef33d42370bce7ae20b02236d221cb0cfb9e6644
Dennis Cheung <hkdennis2k@gmail.com>
2005-07-30 02:49:43 +0000

    Multi-package selection provide api for "Context menu" action, api connection to "Toggle active" button. Gain some performance by reusing UIJob instance,     and replace Thread.sleep(TIME) with UIJob.this.schdeule(TIME)
    TODO: Icon resources are missing, please provide and update plugin.xml

--- src/byecycle/views/ByecycleView.java
+++ src/byecycle/views/ByecycleView.java
@@ -3,8 +3,12 @@
 
 package byecycle.views;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -14,8 +18,10 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -27,6 +33,8 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.PluginActionContributionItem;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.ui.progress.UIJob;
 
@@ -34,201 +42,326 @@ import byecycle.PackageDependencyAnalysis;
 import byecycle.dependencygraph.Node;
 import byecycle.views.layout.GraphCanvas;
 
-public class ByecycleView extends ViewPart implements ISelectionListener, ISelectionProvider {
+public class ByecycleView extends ViewPart implements ISelectionProvider {
 
-	private static final int ONE_MILLISECOND = 1000000;
-	private static final int TEN_SECONDS = 10 * 1000000000;
-	
-	private GraphCanvas _canvas;
-	private IViewSite _site;
-	private Set<ISelectionChangedListener> _selectionListeners = new HashSet<ISelectionChangedListener>();
-	private ISelection _selection;
-	private long _timePackageWasSelected;
-	private long _timeLastLayoutJobStarted;
-	
-	/**
-	 * The constructor.
-	 */
-	public ByecycleView() {
-	}
-	
-	public void init(IViewSite site) throws PartInitException {
-		super.init(site);
-		_site = site;
-		_site.getPage().addSelectionListener(this);
-		_site.setSelectionProvider(this);
-	}
-	
-	public void dispose() {
-		_site.getPage().removeSelectionListener(this);
-		super.dispose();
-	}
+    private static final int ONE_MILLISECOND = 1000000;
 
-	/**
-	 * This is a callback that will allow us to create the viewer and initialize
-	 * it.
-	 */
-	public void createPartControl(Composite parent) {
-		_canvas = new GraphCanvas(parent, new GraphCanvas.Listener() {
-			public void nodeSelected(Node node) {
-				setSelection(node);
-			}
-		});
-		scheduleImproveLayoutJob();
-	}
-	
-	private void scheduleImproveLayoutJob() {
-		UIJob job = new UIJob("package analysis display") {
-			public IStatus runInUIThread(IProgressMonitor monitor) {
-				if (_canvas.isDisposed() || monitor.isCanceled())
-					return Status.OK_STATUS;
+    private static final int TEN_SECONDS = 10 * 1000000000;
 
-				try {
-					_canvas.tryToImproveLayout();
-					Thread.sleep(nanosecondsToSleep() / 1000000);
-				} catch (Exception rx) {
-					rx.printStackTrace(); //Eclipse does not print the stack trace.
-				}
+    public static final int ACTIVACITY = 1;
 
-				scheduleImproveLayoutJob();
-				return Status.OK_STATUS;
-			}
+    public static final String PERSPECTIVE_ID = "byecycle.views.ByecycleView";
 
-		};
-		job.setSystem(true);
-		job.schedule();
-	}
-	
-	private long nanosecondsToSleep() {
-		long currentTime = System.nanoTime();
-		
-		long timeSincePackageWasSelected = currentTime - _timePackageWasSelected; 
-		if (timeSincePackageWasSelected < TEN_SECONDS) return 0;  //Go fast in the first ten seconds.
-		
-		long timeLastLayoutJobTook = currentTime - _timeLastLayoutJobStarted;
-		if (timeLastLayoutJobTook < 0) timeLastLayoutJobTook = 0; //This can happen due to rounding from nanos to millis.
-		
-		long timeToSleep = timeLastLayoutJobTook * 2;  //The more things run in parallel with byecycle, the less greedy byecycle will be. Byecycle is proud to be a very good citizen.  :)
-		if (timeToSleep > TEN_SECONDS) timeToSleep = TEN_SECONDS;
-		if (timeToSleep < ONE_MILLISECOND) timeToSleep = ONE_MILLISECOND;
+    private GraphCanvas _canvas;
 
-		_timeLastLayoutJobStarted = currentTime + timeToSleep;
-		return timeToSleep;
-	}
-	
-	/**
-	 * Passing the focus request to the viewer's control.
-	 */
-	public void setFocus() {
-		//viewer.getControl().setFocus();
-	}
-	
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (!(selection instanceof IStructuredSelection)) {
-			return;
-		}		
-		if (selection.equals(_selection)) {
-			return;
-		}
-		
-		_selection = selection;
-		IStructuredSelection structured = (IStructuredSelection) selection;
-		Object selected = structured.getFirstElement();
-		
-		try {
-			if (selected instanceof IPackageFragment) {
-				IPackageFragment selectedPackage = (IPackageFragment) selected;
-				analyze(selectedPackage.getElementName(), selectedPackage.getCompilationUnits());
-			} else if (selected instanceof ICompilationUnit) {
-				ICompilationUnit compilationUnit = (ICompilationUnit)selected;
-				analyze(compilationUnit.getElementName(), new ICompilationUnit[] { compilationUnit });
-			} else if (selected instanceof IType) {
-				IType type = (IType)selected;
-				analyze(type.getElementName(), new ICompilationUnit[] { type.getCompilationUnit() });
-			}
-		} catch (Exception x) {
-			x.printStackTrace();
-		}
-	}
-	
-	private void analyze(final String elementName, final ICompilationUnit[] compilationUnits) {
-		Job job = new Job("'" + elementName + "' analysis") {
-			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					final Collection<Node<IBinding> > nodes = new PackageDependencyAnalysis(compilationUnits, monitor).dependencyGraph();
-					if (!monitor.isCanceled()) {
-						//dumpGraph(graph);
-						UIJob job = new UIJob("package analysis display") {
-							public IStatus runInUIThread(IProgressMonitor monitor) {
-								try {
-									_canvas.setGraph((Collection<Node>)nodes);
-									_timePackageWasSelected = System.nanoTime();
-								} catch (Exception x) {
-									x.printStackTrace();
-								}
-								return Status.OK_STATUS;
-							}
-						};
-						job.schedule();
-					}
-				} catch (Exception x) {
-					x.printStackTrace();
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		job.schedule();
-		
-	}
+    private IViewSite _site;
 
-	private void dumpGraph(Node[] graph) {
-		System.out.println("*********");
-		for (int i=0; i<graph.length; ++i) {
-			System.out.println(graph[i].name());
-		}
-	}
-	
-	public void addSelectionChangedListener(ISelectionChangedListener listener) {
-		_selectionListeners.add(listener);
-	}
+    private Set<ISelectionChangedListener> _selectionListeners = new HashSet<ISelectionChangedListener>();
 
-	public ISelection getSelection() {
-		return _selection;
-	}
+    private ISelection _selection;
 
-	public void removeSelectionChangedListener(ISelectionChangedListener listener) {
-		_selectionListeners.remove(listener);
-	}
-	
-	void setSelection(Node selection) {
-		if (null == selection) {
-			// drill up
-			IStructuredSelection structured = (IStructuredSelection)selection;
-			IJavaElement element = (IJavaElement)structured.getFirstElement();
-			setSelection(new StructuredSelection(element.getParent()));
-		} else {
-			// drill down
-			Node<IBinding> typedNode = (Node<IBinding>)selection;
-			IBinding binding = typedNode.payload();
-			IJavaElement element = binding.getJavaElement();
-			if (null != element) {
-				setSelection(new StructuredSelection(element));
-			}
-		}
-	}
+    private long _timePackageWasSelected;
 
-	public void setSelection(ISelection selection) {
-		if (selection.equals(_selection)) {
-			return;
-		}
-		fireSelectionChanged(selection);
-	}
+    private long _timeLastLayoutJobStarted;
 
-	private void fireSelectionChanged(ISelection selection) {
-		SelectionChangedEvent event = new SelectionChangedEvent(this, selection);
-		for (ISelectionChangedListener listener : _selectionListeners) {
-			listener.selectionChanged(event);
-		}
-	}
+    private UIJob _job;
+
+    private boolean _pause;
+
+    /**
+     * The constructor.
+     */
+    public ByecycleView() {
+    }
+
+    private void setPaused(boolean pause) {
+        if (pause == _pause)
+            return;
+        _pause = pause;
+        firePropertyChange(ACTIVACITY);
+        ActionContributionItem toolbar = (ActionContributionItem) _site
+                .getActionBars().getToolBarManager().find(
+                        "byecycle.toggleActiveAction");
+        toolbar.getAction().setChecked(pause);
+    }
+
+    public void toggleActive(boolean pause) {
+        if (pause == _pause)
+            return;
+        long currentTime = System.nanoTime();
+        if (_pause) {
+            setPaused(false);
+            _timeLastLayoutJobStarted = currentTime;
+            if (_job.getState() == UIJob.SLEEPING) {
+                _job.wakeUp();
+            } else {
+                scheduleImproveLayoutJob();
+            }
+        } else {
+            _job.sleep();
+            setPaused(true);
+        }
+    }
+
+    @Override
+    public void init(IViewSite site) throws PartInitException {
+        super.init(site);
+        _site = site;
+        // _site.getPage().addSelectionListener(this);
+
+        _site.setSelectionProvider(this);
+
+        _job = new UIJob("package analysis display") {
+            @Override
+            public boolean shouldSchedule() {
+                return !_pause;
+            }
+
+            @Override
+            public boolean shouldRun() {
+                return !_pause;
+            }
+
+            @Override
+            public IStatus runInUIThread(IProgressMonitor monitor) {
+                GraphCanvas canvas = _canvas;
+                if (canvas == null || canvas.isDisposed()
+                        || monitor.isCanceled())
+                    return Status.OK_STATUS;
+                try {
+                    _canvas.tryToImproveLayout();
+                    this.schedule(nanosecondsToSleep() / 1000000);
+                } catch (Exception rx) {
+                    return UIJob.errorStatus(rx);
+                }
+                return Status.OK_STATUS;
+            }
+        };
+        _job.setSystem(true);
+    }
+
+    @Override
+    public void dispose() {
+        // _site.getPage().removeSelectionListener(this);
+        _job.cancel();
+        super.dispose();
+    }
+
+    /**
+     * This is a callback that will allow us to create the viewer and initialize
+     * it.
+     */
+    @Override
+    public void createPartControl(Composite parent) {
+        _canvas = new GraphCanvas(parent, new GraphCanvas.Listener() {
+            public void nodeSelected(Node node) {
+                setSelection(node);
+            }
+        });
+    }
+
+    private void scheduleImproveLayoutJob() {
+        _job.schedule();
+    }
+
+    private long nanosecondsToSleep() {
+        long currentTime = System.nanoTime();
+
+        long timeSincePackageWasSelected = currentTime
+                - _timePackageWasSelected;
+        if (timeSincePackageWasSelected < TEN_SECONDS)
+            return 0; // Go fast in the first ten seconds.
+
+        long timeLastLayoutJobTook = currentTime - _timeLastLayoutJobStarted;
+        if (timeLastLayoutJobTook < 0)
+            timeLastLayoutJobTook = 0; // This can happen due to rounding from
+        // nanos to millis.
+
+        long timeToSleep = timeLastLayoutJobTook * 2; // The more things run
+        // in parallel with
+        // byecycle, the less
+        // greedy byecycle will
+        // be. Byecycle is proud
+        // to be a very good
+        // citizen. :)
+        if (timeToSleep > TEN_SECONDS)
+            timeToSleep = TEN_SECONDS;
+        if (timeToSleep < ONE_MILLISECOND)
+            timeToSleep = ONE_MILLISECOND;
+
+        _timeLastLayoutJobStarted = currentTime + timeToSleep;
+        return timeToSleep;
+    }
+
+    /**
+     * Passing the focus request to the viewer's control.
+     */
+    public void setFocus() {
+        // viewer.getControl().setFocus();
+    }
+
+    @Deprecated
+    public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+        selectionChanged(selection);
+    }
+
+    public void selectionChanged(ISelection selection) {
+        if (!(selection instanceof IStructuredSelection)) {
+            return;
+        }
+        if (selection.equals(_selection)) {
+            return;
+        }
+
+        _selection = selection;
+        IStructuredSelection structured = (IStructuredSelection) selection;
+        Object selected = structured.getFirstElement();
+
+        if (selection.isEmpty())
+            return;
+
+        try {
+            setPaused(true);
+            List<ICompilationUnit> elements = new ArrayList<ICompilationUnit>();
+            for (Iterator i = structured.iterator(); i.hasNext();) {
+                Object item = i.next();
+                if (item instanceof IPackageFragment) {
+                    IPackageFragment selectedPackage = (IPackageFragment) item;
+                    elements.addAll(Arrays.asList(selectedPackage
+                            .getCompilationUnits()));
+                } else if (item instanceof ICompilationUnit) {
+                    ICompilationUnit compilationUnit = (ICompilationUnit) item;
+                    elements.add(compilationUnit);
+                } else if (item instanceof IType) {
+                    IType type = (IType) item;
+                    elements.add(type.getCompilationUnit());
+                } else if (item instanceof IPackageFragmentRoot) {
+                    // TODO: verify and debug this
+                    IPackageFragmentRoot src = (IPackageFragmentRoot) selected;
+                    for (IJavaElement unit : src.getChildren()) {
+                        if (!(unit instanceof IPackageFragment))
+                            continue;
+                        elements.addAll(Arrays.asList(((IPackageFragment) unit)
+                                .getCompilationUnits()));
+                    }
+                }
+                // TODO: can we handle this?
+                // } else if (selected instanceof IJavaProject) {
+                // IJavaProject javaproject = }(IJavaProject) selected;
+                // }
+            }
+            if (!elements.isEmpty()) {
+                if (selected instanceof IPackageFragment) {
+                    IPackageFragment selectedPackage = (IPackageFragment) selected;
+                    setPaused(false);
+                    analyze(selectedPackage.getElementName(), elements
+                            .toArray(new ICompilationUnit[elements.size()]));
+                    scheduleImproveLayoutJob();
+                } else if (selected instanceof ICompilationUnit) {
+                    ICompilationUnit compilationUnit = (ICompilationUnit) selected;
+                    setPaused(false);
+                    analyze(compilationUnit.getElementName(), elements
+                            .toArray(new ICompilationUnit[elements.size()]));
+                    scheduleImproveLayoutJob();
+                } else if (selected instanceof IType) {
+                    IType type = (IType) selected;
+                    setPaused(false);
+                    analyze(type.getElementName(), elements
+                            .toArray(new ICompilationUnit[elements.size()]));
+                    scheduleImproveLayoutJob();
+                }
+            }
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+
+    }
+
+    private void analyze(final String elementName,
+            final ICompilationUnit[] compilationUnits) {
+        Job job = new Job("'" + elementName + "' analysis") {
+            protected IStatus run(IProgressMonitor monitor) {
+                try {
+                    final Collection<Node<IBinding>> nodes = new PackageDependencyAnalysis(
+                            compilationUnits, monitor).dependencyGraph();
+                    if (!monitor.isCanceled()) {
+                        // dumpGraph(graph);
+                        UIJob job = new UIJob("package analysis display") {
+                            public IStatus runInUIThread(
+                                    IProgressMonitor monitor) {
+                                try {
+                                    _canvas.setGraph((Collection<Node>) nodes);
+                                    _timePackageWasSelected = System.nanoTime();
+                                } catch (Exception x) {
+                                    x.printStackTrace();
+                                }
+                                return Status.OK_STATUS;
+                            }
+                        };
+                        job.schedule();
+                    }
+                } catch (Exception x) {
+                    x.printStackTrace();
+                }
+                return Status.OK_STATUS;
+            }
+        };
+        job.schedule();
+    }
+
+    private void dumpGraph(Node[] graph) {
+        System.out.println("*********");
+        for (int i = 0; i < graph.length; ++i) {
+            System.out.println(graph[i].name());
+        }
+    }
+
+    public void addSelectionChangedListener(ISelectionChangedListener listener) {
+        _selectionListeners.add(listener);
+    }
+
+    public ISelection getSelection() {
+        return _selection;
+    }
+
+    public void removeSelectionChangedListener(
+            ISelectionChangedListener listener) {
+        _selectionListeners.remove(listener);
+    }
+
+    void setSelection(Node selection) {
+        if (null == selection) {
+            // drill up
+            IStructuredSelection structured = (IStructuredSelection) selection;
+            IJavaElement element = (IJavaElement) structured.getFirstElement();
+            setSelection(new StructuredSelection(element.getParent()));
+        } else {
+            // drill down
+            Node<IBinding> typedNode = (Node<IBinding>) selection;
+            IBinding binding = typedNode.payload();
+            IJavaElement element = binding.getJavaElement();
+            if (null != element) {
+                setSelection(new StructuredSelection(element));
+            }
+        }
+    }
+
+    public void setSelection(ISelection selection) {
+        if (selection.equals(_selection)) {
+            return;
+        }
+        fireSelectionChanged(selection);
+    }
+
+    private void fireSelectionChanged(ISelection selection) {
+        SelectionChangedEvent event = new SelectionChangedEvent(this, selection);
+        for (ISelectionChangedListener listener : _selectionListeners) {
+            listener.selectionChanged(event);
+        }
+    }
+
+    public boolean isPaused() {
+        return _pause;
+    }
 
 }
