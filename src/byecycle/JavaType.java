fceec27a518e415a9e6123bd6e9b2e80d9f71ba3
Dennis Cheung <hkdennis2k@gmail.com>
2005-08-06 21:06:29 +0000

    Undo Chanes of branch

--- src/byecycle/JavaType.java
+++ src/byecycle/JavaType.java
@@ -1,6 +1,3 @@
-//Copyright (C) 2004 Klaus Wuestefeld and Rodrigo B de Oliveira.
-//This is free software. See the license distributed along with this file.
-
 package byecycle;
 
 import org.eclipse.jdt.core.dom.ITypeBinding;
