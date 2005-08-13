949b0d903444db3719110d8a78fe222ec1c22f33
Dennis Cheung <hkdennis2k@gmail.com>
2005-08-13 20:27:48 +0000

    Fix Annothation in JavaType.valueOf(IBinding)

--- src/byecycle/JavaType.java
+++ src/byecycle/JavaType.java
@@ -7,33 +7,31 @@ import org.eclipse.jdt.ui.ISharedImages;
  * Enum class for 5 standard Java Element Type
  */
 public enum JavaType {
-    PACKAGE(ISharedImages.IMG_OBJS_PACKAGE), // 
-    CLASS(ISharedImages.IMG_OBJS_CLASS), //
-    ANNOTATION(ISharedImages.IMG_OBJS_ANNOTATION), // 
-    INTERFACE(ISharedImages.IMG_OBJS_INTERFACE), // 
-    ENUM(ISharedImages.IMG_OBJS_ENUM);//
+	PACKAGE(ISharedImages.IMG_OBJS_PACKAGE), // 
+	CLASS(ISharedImages.IMG_OBJS_CLASS), //
+	ANNOTATION(ISharedImages.IMG_OBJS_ANNOTATION), // 
+	INTERFACE(ISharedImages.IMG_OBJS_INTERFACE), // 
+	ENUM(ISharedImages.IMG_OBJS_ENUM);//
+	private final String res;
 
-    private final String res;
+	JavaType(String str) {
+		res = str;
+	}
 
-    JavaType(String str) {
-        res = str;
-    }
+	public static JavaType valueOf(ITypeBinding binding) {
+		assert binding != null;
+		if (binding.isAnnotation()) {
+			return ANNOTATION;
+		} else if (binding.isInterface()) {
+			return INTERFACE;
+		} else if (binding.isEnum()) {
+			return ENUM;
+		} else {
+			return CLASS;
+		}
+	}
 
-    public static JavaType valueOf(ITypeBinding binding) {
-        assert binding != null;
-        if (binding.isInterface()) {
-            return JavaType.INTERFACE;
-        } else if (binding.isEnum()) {
-            return ENUM;
-        } else if (binding.isAnnotation()) {
-            return ANNOTATION;
-        } else {
-            return CLASS;
-        }
-    }
-
-    public String getResourceName() {
-        return res;
-    }
-
+	public String getResourceName() {
+		return res;
+	}
 }
