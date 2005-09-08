ae13e8e6bad544a12b645ac77c1502043b1226b5
Dennis Cheung <hkdennis2k@gmail.com>
2005-09-08 20:32:03 +0000

    Add constant key for qualified class name filter

--- src/byecycle/preferences/PreferenceConstants.java
+++ src/byecycle/preferences/PreferenceConstants.java
@@ -4,7 +4,7 @@ package byecycle.preferences;
  * Constant definitions for plug-in preferences.
  */
 public class PreferenceConstants {
+	public static final String P_PACKAGE_EXCLUDES = "packageExcludes";
 
-    public static final String P_PACKAGE_EXCLUDES = "packageExcludes";
-
+	public static final String P_PATTERN_EXCLUDES = "patternExcludes";
 }
