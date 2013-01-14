2bc79f0db3a7ddac0f559ff80e097901a4f7f7a0
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2013-01-14 17:01:29 -0200

    Averaging Node

--- byke-plugin/src/byke/views/layout/algorithm/random/AveragingNode.java
+++ byke-plugin/src/byke/views/layout/algorithm/random/AveragingNode.java
@@ -5,10 +5,26 @@ import org.eclipse.draw2d.geometry.Rectangle;
 import byke.dependencygraph.Node;
 import byke.views.layout.criteria.NodeElement;
 
-public class AveragingNode extends NodeElement {
+class AveragingNode extends NodeElement {
 
-	public AveragingNode(Node<?> node, Rectangle bounds) {
+	private long turns = 0;
+	private double totalX;
+	private double totalY;
+
+	
+	AveragingNode(Node<?> node, Rectangle bounds) {
 		super(node, bounds);
+	}
+
+	
+	void takeAveragePositionDividedBy(int factor) {
+		turns++;
+		totalX += resultingForceX();
+		totalY += resultingForceY();
+		int avgX = (int)(totalX / turns / factor);
+		int avgY = (int)(totalY / turns / factor);
+		System.out.println(node().name() + " " + avgX);
+		position(avgX, avgY);
 	}
 
 }
