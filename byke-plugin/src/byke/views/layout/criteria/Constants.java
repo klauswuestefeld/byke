697280a61327e2897b4ba912c522fca1fcbaf7ce
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2013-07-14 14:13:11 -0300

    DependenciesDown constants experiment.

--- byke-plugin/src/byke/views/layout/criteria/Constants.java
+++ byke-plugin/src/byke/views/layout/criteria/Constants.java
@@ -8,9 +8,9 @@ public class Constants {
 	public static final float NON_CLUTTERING = 30f;
 	public static final float NON_CLUTTERING_DISTANCE = 120f;
 
-	public static final float DEPENDENCY = 15f;
-	public static final float DEPENDENCY_NOT_POINTING_DOWN_FACTOR = 4f;
-	public static final int DEPENDENCY_PREFERRED_SIZE = 300;
+	public static final float DEPENDENCY = 1f; //50f;
+	public static final float DEPENDENCY_NOT_POINTING_DOWN_FACTOR = 1f;
+	public static final int DEPENDENCY_PREFERRED_SIZE = 30;
 
 	public static final float ALPHABETICAL_ORDER_THRUST = 10f;
 }
